6ee565ceba1d64b5cde7a9de9a02e3041f8c41fc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-01 15:24:16 -0800

    Create new app
    The application for team member management

---
+++ app/members/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Member(models.Model):
+    class Role(models.TextChoices):
+        REGULAR = 'regular', 'Regular'
+        ADMIN = 'admin', 'Admin'
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    # The longest international phone number is 15 digits from the below link.
+    # https://en.wikipedia.org/wiki/E.164
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    role = models.CharField(
+        max_length=10,
+        choices=Role.choices,
+        default=Role.REGULAR,
+    )
