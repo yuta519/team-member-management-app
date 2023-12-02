6aa5670d33f2cf9464cb1bb904975a0bd0ef3334
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-02 13:15:57 -0800

    Create Add page
    Roughly implement Add page template, view and urls

--- app/members/urls.py
+++ app/members/urls.py
@@ -5,5 +5,6 @@ from members import views
 app_name = "members"
 
 urlpatterns = [
-    path("", views.MemberListView.as_view(), name="members")
+    path("", views.MemberListView.as_view(), name="list"),
+    path("new", views.MemberCreateView.as_view(), name="create"),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,8 +1,17 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
+from members.forms import MemberForm
 from members.models import Member
 
 
 class MemberListView(generic.ListView):
     model = Member
-    template_name = "members/member_list.html"
+    template_name = "members/list.html"
+
+
+class MemberCreateView(generic.CreateView):
+    model = Member
+    form_class = MemberForm
+    template_name = "members/create.html"
+    success_url = reverse_lazy('members:list')
