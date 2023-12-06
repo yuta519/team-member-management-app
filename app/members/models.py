f7b540288931bbb8c3f8663084aa5b5255c55770
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-06 13:36:25 -0800

    Refactor member model chores
    Fix lines and comment

--- app/members/models.py
+++ app/members/models.py
@@ -8,15 +8,11 @@ class Member(models.Model):
 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    # The longest international phone number is 15 digits from the below link.
+    # The longest international phone number is 15 digits.
     # https://en.wikipedia.org/wiki/E.164
     phone = models.CharField(max_length=15)
     email = models.EmailField()
-    role = models.CharField(
-        max_length=10,
-        choices=Role.choices,
-        default=Role.REGULAR,
-    )
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.REGULAR)
 
     class Meta:
         db_table = "members"
