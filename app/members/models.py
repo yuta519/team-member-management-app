8f644ff2c209e1aae38a7afd65c50f1d2e781b66
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-07 14:04:08 -0800

    Modify Mmeber model validation
    Adds validation for null and blank values when noticed during creating tests

--- app/members/models.py
+++ app/members/models.py
@@ -6,12 +6,12 @@ class Member(models.Model):
         REGULAR = 'regular', 'Regular'
         ADMIN = 'admin', 'Admin'
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
     # The longest international phone number is 15 digits.
     # https://en.wikipedia.org/wiki/E.164
     phone = models.CharField(max_length=15)
-    email = models.EmailField()
+    email = models.EmailField(null=False, blank=False)
     role = models.CharField(max_length=10, choices=Role.choices, default=Role.REGULAR)
 
     class Meta:
