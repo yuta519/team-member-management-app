c90a3e2c493f83f62ab260f7a49f475a4d8b6700
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-12-08 17:10:17 -0800

    Modify member form
    Adds placeholder and classname

--- app/members/forms.py
+++ app/members/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import RadioSelect, TextInput
 
 from members.models import Member
 
@@ -7,3 +8,10 @@ class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = ["first_name", "last_name", "phone", "email", "role"]
+        widgets = {
+            'first_name': TextInput(attrs={'placeholder': 'Enter your first name', 'class': 'form-input'}),
+            'last_name': TextInput(attrs={'placeholder': 'Enter your last name', 'class': 'form-input'}),
+            'phone': TextInput(attrs={'placeholder': 'Enter your phone number', 'class': 'form-input'}),
+            'email': TextInput(attrs={'placeholder': 'Enter your email', 'class': 'form-input'}),
+            'role': RadioSelect(),
+        }
